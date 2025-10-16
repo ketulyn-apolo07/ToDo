cda7a41b8daa7877ec6c3c5b15ff2d743a2dc714
apolodeus do sol-cyber <ketulynsilveira@gmail.com>
2025-10-16 17:19:02 -0300

    minha lista de coisas

--- script.js
+++ script.js
@@ -9,18 +9,51 @@ const btn_nova_tarefa = document.querySelector("#btn-nova-tarefa");
 //procura pelo elemento com o ID "lista-tarefas" no documento HTML
 const lista_tarefas = document.querySelector("#lista-tarefas");
 
+const audioConcluir = new Audio('sound/gmae.wav');
+
 //---------------------------------------------------------------------------------------------------
 //2. FUNÇÃO DE LOGICA
 //---------------------------------------------------------------------------------------------------
 
 function iniciaToDo() {
     //alert ("ola mundo");
-
+    
     //associa função ao evento de clicar no botão de "adicionar" nova tarefa
     btn_nova_tarefa.addEventListener("click", adicionarTarefa);
+    txt_nova_tarefa.addEventListener("keypress", adicionarTarefaEnter)
 }
 function adicionarTarefa() {
-    alert("ola tarefa!");
+    if (txt_nova_tarefa.value.trim() !== "") {
+        const btn_item = `
+            <div>
+                <button class="btn btn-outline-success btn-sm me-2" onclick="concluirTarefa(this)">Concluir</button>
+                <button class="btn btn-outline-danger btn-sm">Excluir</button>
+            </div>     
+        `;
+        
+        //cria um novo item de lista
+        const item = document.createElement("li");
+        item.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center");
+        item.innerHTML = "<span class='w-75 text-truncate'>" + txt_nova_tarefa.value +"</span>" + btn_item;
+        
+        lista_tarefas.append(item); 
+    }
+    txt_nova_tarefa.value ="";
+    txt_nova_tarefa.focus();
+}
+
+function adicionarTarefaEnter(evento) {
+    if (evento.key == "Enter") {
+       adicionarTarefa();
+  }
+}
+
+function concluirTarefa(elemento) {
+    audioConcluir.play();
+
+    for (let i = 0; i <= 40; i++){
+        confetti();
+    }
 }
 //---------------------------------------------------------------------------------------------------
 //3. ESCUTADORES DE EVENTOS E INICIO
